e127e03fa638f9961f49e6aa3400ea13c40e00f4
Guilherme Camposo <guilherme.camposo@me.com>
2020-09-19 11:08:14 -0300

    changing tweet format and adding readme info

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 
 this component is responsible for getting messages from Kafka and post tweets in our "Corporate" Account.
 
- ## How to Deploy in OpenShift
+## Requirements
+
+In order to use this app you need access to a Twitter developer account. For more information on how to get one, see: https://developer.twitter.com/en/apply-for-access
+
+
+## How to Run Locally
+    mvn clean compile quarkus:dev
+
+## How to Deploy in OpenShift
     mvn clean package  -Dquarkus.kubernetes.deploy=true -Dquarkus.kubernetes-client.trust-certs=true -Dquarkus.openshift.expose=true
--- src/main/java/quarkus/hackfest/tweetpublisher/bean/TweetTextBean.java
+++ src/main/java/quarkus/hackfest/tweetpublisher/bean/TweetTextBean.java
@@ -4,17 +4,30 @@ import org.apache.camel.Exchange;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
+import java.util.logging.Logger;
 
 @Singleton
 @Named("tweetTextBean")
 public class TweetTextBean {
 
+    static Logger logger = Logger.getLogger(TweetTextBean.class.getName());
+
     public void generateTweet(Exchange exchange){
 
         String gpName = exchange.getIn().getBody(String.class);
 
-        exchange.getIn().setBody("Who do you think is gonna win #GP"+ gpName + "  ?  #WhoWins");
+        exchange.getIn().setBody("Who do you think is gonna win "+ prepareHashtag(gpName) + " ?  #WhoWins");
 
+    }
+
+    public static String prepareHashtag(String word){
+        StringBuilder hashtag =  new StringBuilder();
+        hashtag.append("#");
+        hashtag.append(word.replaceAll(" ", "").replaceAll("\"", ""));
+
+        logger.info(" Hashtag generated: " + hashtag.toString());
+
+        return  hashtag.toString();
     }
 
 }
